c73e81e25b0e5d37fe0315e13c0575e74b11958b
Kairaa2014 <kairaasharma22@gmail.com>
2025-10-18 12:52:20 +0530

    demo

---
+++ Square Root/sq.py
@@ -0,0 +1,8 @@
+#Ask for input
+number = int(input("What number do you want to find the square root of, it should be an integer:"))
+
+#start importing
+import math
+
+square_root = math.sqrt(number)
+print(f"The square root of", number ,"is:", square_root) 
